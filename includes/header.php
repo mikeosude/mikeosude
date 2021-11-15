73cd915f212af069ae3ec821b1ff244fe01c796c
Michael Ifesinachi Osude <me@mikeosude.com>
2021-11-15 18:58:51 +0100

    h

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,7 @@ include 'entities.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
 <body>
 <div class="topnav">

aa9b06d14d00166d0bebe3813060153a5b63a5a4
Michael Ifesinachi Osude <me@mikeosude.com>
2021-11-15 17:55:57 +0100

    h

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <h2> from footer.php</h2>
 <?php
-echo "<p>Copyright &copy; 1999-" . date("Y") . " mikeosude.com</p>";
+echo "<p>Copyright &copy; 1988-" . date("Y") . " mikeosude.com</p>";
 ?>
 </body>
 </html>

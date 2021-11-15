8eb36ce06842068815bbf452929a4e2d964357f0
Michael Ifesinachi Osude <me@mikeosude.com>
2021-11-15 17:43:01 +0100

    more1

--- includes/header.php
+++ includes/header.php
@@ -9,7 +9,13 @@ include 'entities.php'
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+<div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#news">News</a>
+  <a href="#contact">Contact</a>
+  <a href="#about">About</a>
+</div>
 <h1>from header.php</h1>

dd26787593e01426137a04ece8b7855b5d068856
Michael Ifesinachi Osude <me@mikeosude.com>
2021-11-15 17:22:48 +0100

    Made some major changes
    Major changes done

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Michael Osude</title>
-</head>
+<?php 
+include "./docs/header.php";
+?>
 
-<body>
-The content of the document and testiing coding with working copy
-<p>I am checking to see how quickly my commits are synced</p>
-</body>
 
-</html>
+
+<?php 
+include "./docs/footer.php";
+?>
